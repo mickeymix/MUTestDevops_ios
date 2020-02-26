6b845fbea9229566abae1958e9879a6f63abc321
Chaiaypong <mickey_mix@live.com>
2020-02-26 15:48:14 +0700

    init code ios

---
+++ Pods/Target Support Files/Pods-MUTestDevops/Pods-MUTestDevops-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MUTestDevops : NSObject
+@end
+@implementation PodsDummy_Pods_MUTestDevops
+@end
---
+++ Pods/Target Support Files/Pods-MUTestDevopsTests/Pods-MUTestDevopsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MUTestDevopsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MUTestDevopsTests
+@end
